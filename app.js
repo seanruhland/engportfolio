51bbb4657c26d5b827807ba707c63f9bf5ce8e85
Sean Ruhland <sean.ruhland@gmail.com>
2018-02-16 13:15:58 -0800

    smooth scrolling

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
+$(document).ready(function(){
 
-    console.log('linked')
+	var scrollLink = $('.scroll');
+
+	//Smooth scrolling
+	scrollLink.click(function(e){
+		e.preventDefault();
+		$('body, html').animate({
+			scrollTop:$(this.hash).offset().top
+		}, 2000 );
+	})
+
+	//Active link switching
+	$(window).scroll(function() {
+		var scrollbarLocation= $(this).scrollTop();
+
+		scrollLink.each(function() {
+
+			var sectionOffset= $(this.hash).offset().top
+
+			if (sectionOffset <= scrollbarLocation) {
+				$(this).parent().addClass('active');
+				$(this).parent().siblings().removeClass('active');
+			}
+		})
+	})
+})
